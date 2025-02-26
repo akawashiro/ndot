26d1c630fb57b3962afa8ad2c3d5b5e4bdb73001
Akira Kawata <akirakawata1@gmail.com>
2025-02-27 00:00:51 +0900

    Parse graph

--- src/ast.rs
+++ src/ast.rs
@@ -19,10 +19,10 @@ fn valid_as_id(token: &String) -> bool {
 
 fn parse_id(tokens: &Vec<String>) -> Result<(ID, Vec<String>), String> {
     if tokens.len() == 0 {
-        return Err("no tokens".to_string());
+        return Err(format!("{}:{} No tokens", file!(), line!()));
     }
     if !valid_as_id(&tokens[0]) {
-        return Err("invalid id".to_string());
+        return Err(format!("{}:{} Invalid id", file!(), line!()));
     }
     Ok((
         ID {
@@ -164,7 +164,17 @@ fn test_parse_edge_stmt_op() {
 fn parse_edge_stmt_rhs(tokens: &Vec<String>) -> Result<(EdgeStmtRHS, Vec<String>), String> {
     let (edge_op, rest) = parse_edge_stmt_op(tokens)?;
     let (edge_edge, rest) = parse_edge_stmt_edge(&rest)?;
-    if rest.len() == 0 {
+    let try_rhs = parse_edge_stmt_rhs(&rest);
+    if let Ok((edge_rhs, rest)) = try_rhs {
+        return Ok((
+            EdgeStmtRHS {
+                edge_op,
+                edge_egdge: edge_edge,
+                edge_rhs: Some(Box::new(edge_rhs)),
+            },
+            rest,
+        ));
+    } else {
         return Ok((
             EdgeStmtRHS {
                 edge_op,
@@ -174,15 +184,6 @@ fn parse_edge_stmt_rhs(tokens: &Vec<String>) -> Result<(EdgeStmtRHS, Vec<String>
             rest,
         ));
     }
-    let (edge_rhs, rest) = parse_edge_stmt_rhs(&rest)?;
-    Ok((
-        EdgeStmtRHS {
-            edge_op,
-            edge_egdge: edge_edge,
-            edge_rhs: Some(Box::new(edge_rhs)),
-        },
-        rest,
-    ))
 }
 
 #[test]
@@ -225,7 +226,16 @@ fn test_parse_edge_stmt_rhs() {
 
 fn parse_edge_stmt(tokens: &Vec<String>) -> Result<(EdgeStmt, Vec<String>), String> {
     let (edge_edge, rest) = parse_edge_stmt_edge(tokens)?;
-    if rest.len() == 0 {
+    let try_rhs = parse_edge_stmt_rhs(&rest);
+    if let Ok((edge_rhs, rest)) = try_rhs {
+        return Ok((
+            EdgeStmt {
+                edge_edge,
+                edge_rhs: Some(Box::new(edge_rhs)),
+            },
+            rest,
+        ));
+    } else {
         return Ok((
             EdgeStmt {
                 edge_edge,
@@ -234,14 +244,6 @@ fn parse_edge_stmt(tokens: &Vec<String>) -> Result<(EdgeStmt, Vec<String>), Stri
             rest,
         ));
     }
-    let (edge_rhs, rest) = parse_edge_stmt_rhs(&rest)?;
-    Ok((
-        EdgeStmt {
-            edge_edge,
-            edge_rhs: Some(Box::new(edge_rhs)),
-        },
-        rest,
-    ))
 }
 
 #[test]
@@ -297,6 +299,26 @@ fn test_parse_edge_stmt() {
         None => panic!("expected edge_rhs"),
     }
     assert_eq!(rest, vec![] as Vec<String>);
+
+    let tokens = tokenize("a -> b }".to_string());
+    let (edge_stmt, rest) = parse_edge_stmt(&tokens).unwrap();
+    match edge_stmt.edge_edge {
+        EdgeStmtEdge::NodeID(id) => assert_eq!(id.name, "a"),
+    }
+    match edge_stmt.edge_rhs {
+        Some(rhs) => {
+            match rhs.edge_egdge {
+                EdgeStmtEdge::NodeID(id) => assert_eq!(id.name, "b"),
+            }
+            match rhs.edge_op {
+                EdgeStmtOp::Directed => {}
+                _ => panic!("expected directed"),
+            }
+            assert_eq!(rhs.edge_rhs, None);
+        }
+        None => panic!("expected edge_rhs"),
+    }
+    assert_eq!(rest, vec!["}".to_string()]);
 }
 
 #[derive(Debug, PartialEq)]
@@ -314,7 +336,7 @@ fn parse_stmt(tokens: &Vec<String>) -> Result<(Stmt, Vec<String>), String> {
     if let Ok((edge_stmt, rest)) = try_edge_stmt {
         return Ok((Stmt::EdgeStmt(edge_stmt), rest));
     }
-    Err("expected stmt".to_string())
+    Err(format!("{}:{} Expected stmt. tokens={:?}", file!(), line!(), tokens))
 }
 
 #[test]
@@ -364,23 +386,24 @@ struct StmtList {
 
 fn parse_stmt_list(tokens: &Vec<String>) -> Result<(StmtList, Vec<String>), String> {
     let (stmt, rest) = parse_stmt(tokens)?;
-    if rest.len() == 0 {
-        return Ok((
-            StmtList {
+    let try_stmt_list = parse_stmt_list(&rest);
+    match try_stmt_list {
+        Ok((stmt_list, rest)) => {
+            return Ok((
+                StmtList {
+                    stmt,
+                    stmt_list: Some(Box::new(stmt_list)),
+                },
+                rest,
+            ));
+        }
+        Err(_) => {
+            return Ok((StmtList {
                 stmt,
                 stmt_list: None,
-            },
-            rest,
-        ));
+            }, rest));
+        }
     }
-    let (stmt_list, rest) = parse_stmt_list(&rest)?;
-    Ok((
-        StmtList {
-            stmt,
-            stmt_list: Some(Box::new(stmt_list)),
-        },
-        rest,
-    ))
 }
 
 #[test]
@@ -395,6 +418,107 @@ fn test_parse_stmt_list() {
         _ => panic!("expected IDEqStmt"),
     }
     match stmt_list.stmt_list {
+        Some(stmt_list) => {
+            match stmt_list.stmt {
+                Stmt::EdgeStmt(edge_stmt) => {
+                    match edge_stmt.edge_edge {
+                        EdgeStmtEdge::NodeID(id) => assert_eq!(id.name, "a"),
+                    }
+                    match edge_stmt.edge_rhs {
+                        Some(rhs) => {
+                            match rhs.edge_egdge {
+                                EdgeStmtEdge::NodeID(id) => assert_eq!(id.name, "b"),
+                            }
+                            match rhs.edge_op {
+                                EdgeStmtOp::Undirected => {}
+                                _ => panic!("expected undirected"),
+                            }
+                            assert_eq!(rhs.edge_rhs, None);
+                        }
+                        None => panic!("expected edge_rhs"),
+                    }
+                }
+                _ => panic!("expected EdgeStmt"),
+            }
+            assert_eq!(stmt_list.stmt_list, None);
+        }
+        None => panic!("expected stmt_list"),
+    }
+    assert_eq!(rest, vec![] as Vec<String>);
+}
+
+struct Graph {
+    strict: bool,
+    is_digraph: bool,
+    stmt_list: StmtList,
+}
+
+fn parse_graph(tokens: &Vec<String>) -> Result<(Graph, Vec<String>), String> {
+    let mut rest = tokens.clone();
+    let mut strict = false;
+    if rest.len() > 0 && rest[0].to_lowercase() == "strict" {
+        strict = true;
+        rest = rest[1..].to_vec();
+    }
+    if rest.len() == 0 {
+        return Err("no tokens".to_string());
+    }
+    let is_digraph = match rest[0].to_lowercase().as_str() {
+        "graph" => false,
+        "digraph" => true,
+        _ => return Err("expected graph or digraph".to_string()),
+    };
+    rest = rest[1..].to_vec();
+    if rest.len() == 0 {
+        return Err("no tokens".to_string());
+    }
+    match rest[0].as_str() {
+        "{" => {}
+        _ => return Err("expected {".to_string()),
+    }
+    rest = rest[1..].to_vec();
+    if rest.len() == 0 {
+        return Err("no tokens".to_string());
+    }
+    let (stmt_list, mut rest) = parse_stmt_list(&rest[0..].to_vec())?;
+    if rest.len() == 0 {
+        return Err("no tokens".to_string());
+    }
+    match rest[0].as_str() {
+        "}" => {}
+        _ => return Err("expected }".to_string()),
+    }
+    rest = rest[1..].to_vec();
+    Ok((
+        Graph {
+            strict,
+            is_digraph,
+            stmt_list,
+        },
+        rest,
+    ))
+}
+
+#[test]
+fn test_parse_graph() {
+    let tokens = tokenize(
+        r#"graph {
+    a = b
+    a -- b
+}"#
+        .to_string(),
+    );
+    let (graph, rest) = parse_graph(&tokens).unwrap();
+    assert_eq!(graph.strict, false);
+    assert_eq!(graph.is_digraph, false);
+    match graph.stmt_list.stmt {
+        Stmt::IDEqStmt(id_eq_stmt) => {
+            assert_eq!(id_eq_stmt.id_left.name, "a");
+            assert_eq!(id_eq_stmt.id_right.name, "b");
+        }
+        _ => panic!("expected IDEqStmt"),
+    }
+    match graph.stmt_list.stmt_list {
         Some(stmt_list) => {
             match stmt_list.stmt {
                 Stmt::EdgeStmt(edge_stmt) => {
