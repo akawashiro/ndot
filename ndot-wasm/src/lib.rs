18ece2f52a0bf8d9db1172d421585fe9eeb93419
Akira Kawata <akirakawata1@gmail.com>
2025-03-11 23:15:56 +0900

    Delete warning

--- ndot-wasm/src/lib.rs
+++ ndot-wasm/src/lib.rs
@@ -1,9 +1,9 @@
 mod utils;
 
-use ndot::*;
+use ndot::make_svg_from_dot as make_svg_from_dot_rust;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn make_svg_from_dot(dot: String) -> Result<String, String> {
-    ndot::make_svg_from_dot(dot)
+    make_svg_from_dot_rust(dot)
 }
