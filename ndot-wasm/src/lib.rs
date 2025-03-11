93fa6453de64c2222931cebd3f00467bd0402569
Akira Kawata <akirakawata1@gmail.com>
2025-03-11 23:26:43 +0900

    Make DotResult

--- ndot-wasm/src/lib.rs
+++ ndot-wasm/src/lib.rs
@@ -3,7 +3,23 @@ mod utils;
 use ndot::make_svg_from_dot as make_svg_from_dot_rust;
 use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen(getter_with_clone)]
+pub struct DotResult {
+    pub svg: Option<String>,
+    pub error: Option<String>,
+}
+
 #[wasm_bindgen]
-pub fn make_svg_from_dot(dot: String) -> Result<String, String> {
-    make_svg_from_dot_rust(dot)
+pub fn make_svg_from_dot(dot: String) -> DotResult {
+    let r = make_svg_from_dot_rust(dot);
+    match r {
+        Ok(svg) => DotResult {
+            svg: Some(svg),
+            error: None,
+        },
+        Err(e) => DotResult {
+            svg: None,
+            error: Some(e),
+        },
+    }
 }
